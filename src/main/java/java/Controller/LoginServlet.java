b92e0405c758a260a8724cbc622c214aa614fa6a
User <User@172.17.132.54>
2025-06-16 17:34:24 +0800

    initial commit

--- src/main/java/java/Controller/LoginServlet.java
+++ src/main/java/java/Controller/LoginServlet.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package Controller;
+package java.Controller;
 
-import Dao.UserDAO;
-import Model.User;
+import java.Dao.UserDAO;
+import java.Model.User;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
--- src/main/java/java/Model/Task.java
+++ src/main/java/java/Model/Task.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package java.Model;
 
-import Model.User;
+import java.Model.User;
 
 public class Task {
 
